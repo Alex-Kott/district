1a01bdfbc0504c7b08fc3b858b4753d828a80974
AlexKott <alexkott@gmail.com>
2017-08-22 19:10:05 +0300

    first commit

---
+++ parser.py
@@ -0,0 +1,74 @@
+import requests as req
+import json
+import xlrd
+import csv
+import time
+
+workbook = xlrd.open_workbook("data.xlsx")
+sheet = workbook.sheet_by_index(0)
+
+url = "https://geocode-maps.yandex.ru/1.x/"
+
+with open("result.csv", "w") as f:
+	fieldnames = ['Полное название', 'Категории', 'Город', 'Адрес', 'Vkontakte', 'тел.', 'Район']
+	writer = csv.writer(f, delimiter=";")
+	writer.writerow(fieldnames)
+	f.close()
+
+for row in range(1, sheet.nrows):
+	cols = sheet.row_values(row)
+	
+	#print(cols)
+
+	line = []
+	for j in range(6):
+		line.append(cols[j])
+
+	city = cols[2]
+	address = cols[3]
+	#print("{}, {}".format(city, address))
+
+	params = {
+		'format'  : 'json',
+		'geocode' :	"{}, {}".format(city, address)
+	}
+
+	try:
+		r = req.get(url, params = params)
+	except Exception as e:
+		print(str(e))
+		time.sleep(20)
+		row = row - 1
+		continue
+	resp = json.loads(r.text)
+	obj = resp['response']['GeoObjectCollection']['featureMember'][0]
+	lng, lat = obj['GeoObject']['Point']['pos'].split(" ") # lng, lat
+
+	params = {
+		'format'	: 'json',
+		'geocode' 	: '{},{}'.format(lng, lat),
+		'kind'		: 'district'
+	}
+	try:
+		r = req.get(url, params = params)
+	except Exception as e:
+		print(str(e))
+		time.sleep(20)
+		row = row - 1
+		continue
+
+	resp = json.loads(r.text)
+	try:
+		featureMember = resp['response']['GeoObjectCollection']['featureMember'][0]
+	except:
+		print(city, address)
+	components = featureMember['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
+	for comp in components:
+		if comp['kind'] == 'district':
+			district = comp['name']
+			line.append(district)
+
+	with open("result.csv", "a") as f:
+		writer = csv.writer(f, delimiter=";")
+		writer.writerow(line)
+		f.close()
