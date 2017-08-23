2e4a60a07e8aa0722f463b40978f3b0564332748
AlexKott <alexkott@gmail.com>
2017-08-23 15:07:54 +0300

    парсер споткнулся на 11788

--- parser.py
+++ parser.py
@@ -3,20 +3,29 @@ import json
 import xlrd
 import csv
 import time
+import re
 
 workbook = xlrd.open_workbook("data.xlsx")
 sheet = workbook.sheet_by_index(0)
 
+not_found = []
 url = "https://geocode-maps.yandex.ru/1.x/"
-
+'''
 with open("result.csv", "w") as f:
 	fieldnames = ['Полное название', 'Категории', 'Город', 'Адрес', 'Vkontakte', 'тел.', 'Район']
 	writer = csv.writer(f, delimiter=";")
 	writer.writerow(fieldnames)
-	f.close()
+	f.close()'''
+
+count = 1
+
+border = 1
 
 for row in range(1, sheet.nrows):
 	cols = sheet.row_values(row)
+	border += 1
+	if border < 11788:
+		continue
 	
 	#print(cols)
 
@@ -26,6 +35,7 @@ for row in range(1, sheet.nrows):
 
 	city = cols[2]
 	address = cols[3]
+	district = ''
 	#print("{}, {}".format(city, address))
 
 	params = {
@@ -37,7 +47,7 @@ for row in range(1, sheet.nrows):
 		r = req.get(url, params = params)
 	except Exception as e:
 		print(str(e))
-		time.sleep(20)
+		time.sleep(10)
 		row = row - 1
 		continue
 	resp = json.loads(r.text)
@@ -53,22 +63,34 @@ for row in range(1, sheet.nrows):
 		r = req.get(url, params = params)
 	except Exception as e:
 		print(str(e))
-		time.sleep(20)
+		time.sleep(10)
 		row = row - 1
 		continue
 
 	resp = json.loads(r.text)
-	try:
-		featureMember = resp['response']['GeoObjectCollection']['featureMember'][0]
-	except:
-		print(city, address)
-	components = featureMember['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
-	for comp in components:
-		if comp['kind'] == 'district':
-			district = comp['name']
-			line.append(district)
+	
+	featureMembers = resp['response']['GeoObjectCollection']['featureMember']
+	if len(featureMembers) == 0:
+		not_found.append((city, address))
+
+	for item in featureMembers:
+		components = item['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
+		for comp in components:
+			if comp['kind'] == 'district':
+				buff = re.findall(r'(\sрайон$)|(\sокруг$)', comp['name'])
+				if len(buff) > 0:
+					district = comp['name']
+					
+	line.append(district)
 
 	with open("result.csv", "a") as f:
 		writer = csv.writer(f, delimiter=";")
 		writer.writerow(line)
 		f.close()
+
+	count = count + 1
+	print("{} / 12601".format(count), end='\r', flush=True)
+
+with open("not_found.txt", "w") as nff:
+	nff.write(not_found)
+	nff.close()
